7be33e4a82e9630fb92ad6bdedf779481c99a79f
Legedith <jatindehmiwal@gmail.com>
2019-08-08 23:57:40 +0530

    started making long nummber adding program

---
+++ add really long numbers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <conio,h>
+#include <mistack.h>
+using namespace std;
+
+int main()
+{
+	string num1;
+	string num2;
+	getline(cin,num1);
+	getline(cin,num2);
+	stack<char> s1(num1.length());
+	stack<char> s1(num1.length());
+	for(int i=0; i<)
+	
+}
+
