09cfdacf52552e61e429dc5dbcccfeed6f4ae50b
Legedith <jatindehmiwal@gmail.com>
2019-08-12 21:54:54 +0530

    added ans stack

--- add really long numbers.cpp
+++ add really long numbers.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <conio.h>
 #include "mistack.h"
-//#include "reverse.h"
 using namespace std;
 
 int main()
@@ -10,16 +9,19 @@ int main()
 	string num2;
 	getline(cin,num1);
 	getline(cin,num2);
-//	num1 = rev(num1);
-//	num2 = rev(num2);
 	stack<char> s1(num1.length());
 	stack<char> s2(num2.length());
-	
+	stack<char> ans(s1.length()>s2.length()? s1.length()+1:s2.length()+1);
+	char carry = '0'-48;
 	for(int i=0; i<num1.length(); i++)
 		s1.push(num1[i]);
 	for(int i=0; i<num2.length(); i++)
 		s2.push(num2[i]);	
 	
+	while(!s1.isEmpty() && !s2.isEmpty())
+	{
+		
+	}
 	getch();
 	return 0;
 	
