4a85fd132777e20367aba0bdd16727b8416188c7
legedith <jatindehmiwal@gmail.com>
2019-08-13 23:18:53 +0530

    Singly linked list added class

--- SLL.cpp
+++ SLL.cpp
@@ -11,3 +11,34 @@ struct node{
 		info = data;
 	}
 };
+
+template <class type>
+class sll{
+	public:
+		node<type>* head;
+		void insert();		
+		/*
+		Insert 
+			end
+			begin
+		deleter
+			end 
+			beg
+		reverse
+		print
+		*/
+		
+};
+
+template <class type>
+void sll<type>::insert()
+{
+	
+}
+int main()
+{
+	node<int> a(5); 
+	getch()l;
+	return 0;
+}
+
--- add really long numbers.cpp
+++ add really long numbers.cpp
@@ -80,7 +80,6 @@ int main()
 	}
 	getch();
 	return 0;
-	
 }
 
 
