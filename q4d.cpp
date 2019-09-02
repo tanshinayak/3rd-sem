466ae512c75a23996119e78b529d042ffcc45f25
Legedith <jatindehmiwal@gmail.com>
2019-09-02 11:11:42 +0530

    producer

--- q4d.cpp
+++ q4d.cpp
@@ -1,23 +1,22 @@
 #include<iostream>
 #include<sys/types.h>
 #include<unistd.h>
+#include <sys/wait.h>
 using namespace std;
  int main()
  {
    pid_t pid;
    pid=fork();
+   int a = wait(NULL);
     if(pid<0)
      {
       cout<<"Error no process "<<endl;
       }
        else if(pid==0)
         {
-         cout<<"child process       :"<<getppid()<<" "<<pid<<" "<<getpid()<<endl;
- 		return 0;
+         cout<<"child process       :"<<getppid()<<" "<<pid<<" "<<getpid()<<" "<<a<<endl;
          }
          else
-         {
-           cout<<"parent process    :"<<getppid()<<" "<<pid<<" "<<getpid()<<endl;
+           cout<<"parent process    :"<<getppid()<<" "<<pid<<" "<<getpid()<<" "<<a<<endl;
      return 0;
    }
-}
